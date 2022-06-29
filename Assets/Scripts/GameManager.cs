f7a3cad2b8d4a3209d2af12b7596a79413efe9be
Dev-PunChi <dev.punchi.84@gmail.com>
2022-06-29 18:04:10 +0900

    Update cmera add

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -10,6 +10,10 @@ public class GameManager : MonoBehaviour
     [DllImport("__Internal")]
     private static extern void CallReact(string userName, int score);
 
+    public Camera cam;
+    private Vector3 _position;
+    private Quaternion _quaternion;
+
     // Start is called before the first frame update
     public int Score
     {
@@ -36,5 +40,34 @@ public class GameManager : MonoBehaviour
     {
         Score++;
         UnityCall();
+    }
+
+    public void CameraMove()
+    {
+        Debug.Log("Camera Move");
+        _position = new Vector3(111.818f, 23.80329f, 22.51221f);
+        _quaternion = Quaternion.Euler(new Vector3(63.014f, -179.346f, 0));
+    }
+
+    private void Awake()
+    {
+        _position = Vector3.zero;
+        _quaternion = Quaternion.identity;
+    }
+
+
+    private void LateUpdate()
+    {
+        if (_position != Vector3.zero)
+        {
+            cam.transform.position = _position;
+            _position = Vector3.zero;
+        }
+        if (_quaternion != Quaternion.identity)
+        {
+            cam.transform.rotation = _quaternion;
+            _quaternion = Quaternion.identity;
+        }
+
     }
 }
